5727ff5316bf5d7ace144516554c9e2b5de806be
Paulo-Alvares <paulo.alvares@sptech.school>
2024-07-14 20:27:55 -0300

    feature: starting repository

---
+++ Java/src/main/java/designPattern/builder/problema/TestePessoa.java
@@ -0,0 +1,17 @@
+package designPattern.builder.problema;
+
+public class TestePessoa {
+
+    public static void main(String[] args) {
+        // construtor de pessoa foi marcado como privado
+        /*Pessoa pessoa = new Pessoa("Anderson",
+                "Piotto",
+                "09876554323",
+                "piottok10@gmail.com",
+                "careca",
+                LocalDate.of(1985,03,12));*/
+
+        //System.out.println(pessoa);
+
+    }
+}
